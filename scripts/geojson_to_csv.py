b945acd65142deb0ed21d9ea2a31abadc45a2e25
xhon9 <ivangallo99@gmail.com>
2022-10-28 16:13:20 +0200

    create csv for task 2-3-4 (top trees distribution per neighborhood)

--- scripts/geojson_to_csv.py
+++ scripts/geojson_to_csv.py
@@ -1,4 +1,9 @@
+import json
+import pandas as pd
 import geopandas as gpd
+
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
 
 DATA_PATH = '../data'
 
@@ -23,3 +28,44 @@ top_trees['Mean Canopy Cover (m2)'] = canopy_cover.groupby('Name').mean().round(
 top_trees = top_trees.sort_values(by='Count', ascending=False)[:top]
 top_trees.to_csv(f'{DATA_PATH}/top_{top}_trees.csv', index=False)
 
+# Task 2-3-4
+'''
+stacked bar chart comparing neighborhood and tree types (top 5 + others)
+'''
+top = 5
+
+# load "circoscrizioni" file (neighborhoods) and create a map
+nbhs_map = {}
+with open('../data/circoscrizioni.json', 'r') as neighborhoods:
+    nbhs = json.load(neighborhoods)
+
+    for nbh in nbhs['features']:
+        nbhs_map[nbh['properties']['nome']] = Polygon(nbh['geometry']['coordinates'][0])
+
+# define a function to retrieve for a given point the name of the neighborhood
+def point_to_neighborhood(point):
+    for nbh_name, nbh_points in nbhs_map.items():
+        if nbh_points.contains(point): return nbh_name
+
+data = df[['Longitude', 'Latitude', 'Name']][:-1] # remove the last line because is the total
+
+# retrieve the neighborhood from the longitude and latitude
+for nbh_name, nbh_points in nbhs_map.items():
+    data['Neighborhood'] = data.apply(lambda x: point_to_neighborhood(Point(x.Longitude, x.Latitude)), axis=1)
+
+data = data[['Name', 'Neighborhood']]
+
+# keep only the top X trees generalizing the others
+all_trees = df['Name'].unique()
+top_X_trees = list(top_trees[:top]['Name'])
+other_trees = list(set(all_trees) - set(top_X_trees))
+
+data['Name'] = data['Name'].map({tree :(tree if tree in top_X_trees else 'Other') for tree in all_trees})
+data['Count'] = 1
+
+# pivot the dataframe and fill the holes
+pivot_data = pd.pivot_table(data, index='Neighborhood', columns='Name', values='Count', aggfunc=sum).fillna(0).astype(int).reset_index()
+# order columns following the order of the top X trees (just because my brain wants to see them ordered)
+pivot_data = pivot_data[['Neighborhood'] + top_X_trees + ['Other']]
+# save into csv
+pivot_data.to_csv('../data/top_trees_neighborhood.csv', index=False)

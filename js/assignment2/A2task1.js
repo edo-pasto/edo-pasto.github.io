c1f6f8f1ad7b423b949630ab856d7b9498bbf1af
Edoardo Pastorino <edo@Edoardos-MBP.homenet.telecomitalia.it>
2022-11-09 21:45:04 +0100

    inserted a dropdown menu for the first task of ass2 and tried to put tooltip in the firts chart (ass2) without any success :)

--- js/assignment2/A2task1.js
+++ js/assignment2/A2task1.js
@@ -4,7 +4,7 @@ var margin = {top: 20, right: 30, bottom: 30, left: 320},
     height = 420 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
-var svg = d3.select("#A2task1")
+var svgA2T1 = d3.select("#A2task1")
   .append("svg")
     // .attr("width", width + margin.left + margin.right)
     // .attr("height", height + margin.top + margin.bottom)
@@ -13,6 +13,7 @@ var svg = d3.select("#A2task1")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
+
 // get the data
 d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/1_OneNum.csv", function(data) {
 
@@ -20,46 +21,46 @@ d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_data
   var x = d3.scaleLinear()
       .domain([0, 1000])     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
       .range([0, width]);
-  svg.append("g")
+      svgA2T1.append("g")
       .attr("transform", "translate(0," + height + ")")
       .call(d3.axisBottom(x));
 
   // Y axis: initialization
   var y = d3.scaleLinear()
       .range([height, 0]);
-  var yAxis = svg.append("g")
+  var yAxis = svgA2T1.append("g")
 
 
-  var tooltipA2T1 = d3.select("#task1")
-  .append("div")
-  .style("opacity", 0)
-  .attr("class", "tooltip")
-  .style("background-color", "white")
-  .style("border", "solid")
-  .style("border-width", "2px")
-  .style("border-radius", "5px")
-  .style("padding", "10px")
+//   var tooltipA2T1 = d3.select("#A2task1")
+//   .append("div")
+//   .style("opacity", 0)
+//   .attr("class", "tooltip")
+//   .style("background-color", "white")
+//   .style("border", "solid")
+//   .style("border-width", "2px")
+//   .style("border-radius", "5px")
+//   .style("padding", "10px")
 
-// Three function that change the tooltip when user hover / move / leave a cell
-var mouseoverA2T1 = function (d) {
-  // const totalAmount = d.Count;
-  // const treeType = d.Name;
-  // const canopyMean = d["Mean Canopy Cover (m2)"];
-  // tooltipA2T1
-  //     .html("Tree Type: " + treeType + "<br>" + "Total Amount: " + totalAmount + "<br>" + "Canopy mean: " + canopyMean)
-  //     .style("opacity", 1);
-      d3.select(this).attr("fill", "#0e6efc");
-}
-var mousemoveA2T1 = function (d) {
-  tooltip
-  .style('left', (event.pageX+40) + 'px')
-  .style('top', (event.pageY+5) + 'px')
-}
-var mouseleaveA2T1 = function (d) {
-  tooltipA2T1
-      .style("opacity", 0);
-      d3.select(this).attr("fill", "#69b3a2");
-}
+// // Three function that change the tooltip when user hover / move / leave a cell
+// var mouseoverA2T1 = function (d) {
+//   // const totalAmount = d.Count;
+//   // const treeType = d.Name;
+//   // const canopyMean = d["Mean Canopy Cover (m2)"];
+//   tooltipA2T1
+//       .html("Tree Type: " + treeType + "<br>" + "Total Amount: " + totalAmount + "<br>" + "Canopy mean: " + canopyMean)
+//       .style("opacity", 1);
+//       d3.select(this).attr("fill", "#0e6efc");
+// }
+// var mousemoveA2T1 = function (d) {
+//   tooltipA2T1
+//   .style('left', (event.pageX+40) + 'px')
+//   .style('top', (event.pageY+5) + 'px')
+// }
+// var mouseleaveA2T1 = function (d) {
+//   tooltipA2T1
+//       .style("opacity", 0);
+//       d3.select(this).attr("fill", "#69b3a2");
+// }
 
   // A function that builds the graph for a specific value of bin
   function update(nBin) {
@@ -81,8 +82,11 @@ var mouseleaveA2T1 = function (d) {
         .call(d3.axisLeft(y));
 
     // Join the rect with the bins data
-    var u = svg.selectAll("rect")
-        .data(bins)
+    var u = svgA2T1.selectAll("rect")
+    // .on("mouseover", mouseoverA2T1)
+    // .on("mousemove", mousemoveA2T1)
+    // .on("mouseleave", mouseleaveA2T1)
+        .data(bins);
 
     // Manage the existing bars and eventually the new ones:
     u
@@ -96,9 +100,9 @@ var mouseleaveA2T1 = function (d) {
           .attr("width", function(d) { return x(d.x1) - x(d.x0) -1 ; })
           .attr("height", function(d) { return height - y(d.length); })
           .style("fill", "#69b3a2")
-          // .on("mouseover", mouseoverA2T1)
-          // .on("mousemove", mousemoveA2T1)
-          // .on("mouseleave", mouseleaveA2T1)
+ 
+
+          
 
 
     // If less bar in the new histogram, I delete the ones not in use anymore

f142baaaf94d547a98710ea9fa38a8171bbb0ef0
xhon9 <ivangallo99@gmail.com>
2022-12-06 11:20:00 +0100

    fix year_list with the required ones

--- data/temp_data/clean_temp_data.py
+++ data/temp_data/clean_temp_data.py
@@ -10,8 +10,10 @@ df = pd.read_excel('daily_temp_salorono.xlsx')[:-1]
 # cast date into real datetime object
 df['date'] = df['date'].apply(lambda x: pd.to_datetime(x.replace('.','-'), dayfirst=True))
 
-# filter keeping only from 2015 until now
-df = df[df['date'] >= '2015-01-01']
+# filter keeping only required year
+year_list = [2021, 2017, 2013, 2009, 2005, 2001, 1997, 1993]
+df['year'] = df['date'].apply(lambda x: x.year)
+df = df[df['year'].isin(year_list)]
 
 # clean non numeric data in "sum", "min", "max"
 df['min'] = df['min'].apply(lambda x: float(x) if type(x)==int else 0.0 if x=='---' else float(x))
